b4fb208a06142c8a2443ec2fdc47db9159c320d7
ignatovivin <ignatov@a-3.ru>
2026-01-27 18:12:20 +0500

    Fix responsive layouts for 768px and 1024px breakpoints

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,6 @@ import './Header.css'
 import Button from './Button'
 import { useState, useEffect } from 'react'
 
-function PlusIcon() {
-  return (
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M8 3V13M3 8H13" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round"/>
-    </svg>
-  )
-}
-
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false)
   const [isDark, setIsDark] = useState(false)
